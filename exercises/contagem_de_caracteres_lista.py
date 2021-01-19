d1b8a2779a0ed98bfc1efa159eae5ae583c03799
r0bertwell <r0bertwell@hotmail.com>
2021-01-19 12:11:32 -0300

    função que conta caracteres de uma lista

---
+++ exercises/contagem_de_caracteres_lista.py
@@ -0,0 +1,9 @@
+def contar_caracteres(s):
+    ''' FUNÇÃO QUE CONTA OS CARACTERES DE UMA STRING
+    :param s: string a ser contada
+    '''
+    num_of_caracteres = {}
+    for caracter in s:
+        num_of_caracteres[caracter] = (num_of_caracteres.get(caracter, 0) + 1)
+
+    return num_of_caracteres
