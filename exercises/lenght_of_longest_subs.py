79a13bb51b7697e27472ad8b4b6d125b860a526a
Wellington Roberto <65924911+R0bertWell@users.noreply.github.com>
2021-01-20 17:49:45 -0300

    length of longest substring without rep
    Time complexity O(n)

--- exercises/lenght_of_longest_subs.py
+++ exercises/lenght_of_longest_subs.py
@@ -1,13 +1,13 @@
 def length_of_longest_substring(s):
-    dicts = {}
-    maxlength = start = 0
+    dici = {}
+    max_s_rep = repeticao_na_fila = 0
     for i, value in enumerate(s):
-        if value in dicts:
-            sums = dicts[value] + 1
-            if sums > start:
-                start = sums
-        num = i - start + 1
-        if num > maxlength:
-            maxlength = num
-        dicts[value] = i
-    return maxlength
+        if value in dici:
+            num_repetidos = dici[value] + 1
+            if num_repetidos > repeticao_na_fila:
+                repeticao_na_fila = num_repetidos
+        num_repeticoes = i + 1 - repeticao_na_fila
+        if num_repeticoes > max_s_rep:
+            max_s_rep = num_repeticoes
+        dici[value] = i
+    return max_s_rep
